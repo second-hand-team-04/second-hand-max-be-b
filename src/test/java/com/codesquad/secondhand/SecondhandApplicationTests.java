9ffdfca60e7175554f46047636a07b618a86cc85
road-jin <rjswmtk@gmail.com>
2023-08-23 15:59:39 +0900

    build: 프로젝트 초기 세팅

---
+++ src/test/java/com/codesquad/secondhand/SecondhandApplicationTests.java
@@ -0,0 +1,12 @@
+package com.codesquad.secondhand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondhandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
