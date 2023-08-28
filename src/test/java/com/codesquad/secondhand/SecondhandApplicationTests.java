2d766ae5b2032b1e73aa7044a490babe5e2daf8d
road-jin <rjswmtk@gmail.com>
2023-08-28 09:43:51 +0900

    test #6: Application 테스트 profile 설정 추가

--- src/test/java/com/codesquad/secondhand/SecondhandApplicationTests.java
+++ src/test/java/com/codesquad/secondhand/SecondhandApplicationTests.java
@@ -2,8 +2,10 @@ package com.codesquad.secondhand;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SecondhandApplicationTests {
 
 	@Test
