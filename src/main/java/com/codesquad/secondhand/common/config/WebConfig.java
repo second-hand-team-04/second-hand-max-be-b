72fc6c54df559165a9d3dd94155a2342db966093
road-jin <rjswmtk@gmail.com>
2023-08-31 16:59:14 +0900

    fix #14: cors bug

--- src/main/java/com/codesquad/secondhand/common/config/WebConfig.java
+++ src/main/java/com/codesquad/secondhand/common/config/WebConfig.java
@@ -1,9 +1,11 @@
 package com.codesquad.secondhand.common.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
 
 	@Override
